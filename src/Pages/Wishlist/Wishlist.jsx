82e18e341e5027f8a82f12f87a5b9fff45a64a47
Mohammad-Hasham <hashamhashimi80@gmail.com>
2026-07-14 22:55:41 +0430

    working on washlist

--- src/Pages/Wishlist/Wishlist.jsx
+++ src/Pages/Wishlist/Wishlist.jsx
@@ -1,5 +1,6 @@
 
 import './Wishlist.css'
+import smartWatch from "../../assets/Images/smartWatch.png"
 
 
 function Wishlist (){
@@ -7,22 +8,25 @@ function Wishlist (){
     <section className="wishlist">
 <h2>My Wishlist</h2>
 <div className="wishlist-title ">
-<p>Product Name</p>
+<input type="checkbox" />
+<p>Product</p>
+<p>Quantity</p>
 <p>Unite price</p>
 <p> Stock Stutes</p>
 <p>Action</p>
 </div>
-<div className="wishlist-product"> 
-<button>X</button>
-<img src="" alt="" />
+<div className="wishlist-product">
+<input type="checkbox" />
+<img src={smartWatch} alt="" />
 <p>jacket</p>
 <span>700$</span>
 <span> in Stock</span>
+<div className='wishlist-btn'>
+<button>Eye</button>
 <button>Add to Card</button>
+<button>Trush</button>
 </div>
-
-
-
+</div>
 
     </section>
     )
