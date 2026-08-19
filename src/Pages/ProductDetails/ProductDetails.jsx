d8b5b879524aa1e6c455803ddc0e90da8021aad9
Mohammad-Hasham <hashamhashimi80@gmail.com>
2026-08-19 23:48:29 +0430

    product page

---
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,16 @@
+function productdetails (){
+    return(
+        <section className="product-details">
+            <div className="imges-slide">
+
+            </div>
+            <div className="product-details-img">
+
+            </div>
+            <div className="product-details-informations">
+        <div></div>
+            </div>
+
+        </section>
+    )
+}
