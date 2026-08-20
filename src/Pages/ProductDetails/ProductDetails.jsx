9c373927c4743d04c7f1b5e251c474cd3c2baa2e
Mohammad-Hasham <hashamhashimi80@gmail.com>
2026-08-20 23:59:12 +0430

    product details page going to finish

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,4 +1,4 @@
-function productdetails (){
+function ProductDetails (){
     return(
         <section className="product-details">
             <div className="imges-slide">
@@ -8,9 +8,32 @@ function productdetails (){
 
             </div>
             <div className="product-details-informations">
-        <div></div>
+            <h2> product name </h2>
+            <div className="product-information-rate">
+
+            </div>
+            <div className="product-information-price">
+
+            </div>
+            <p></p>
+            <dl className="products-informations-deals">
+                <dd>Brand:</dd>
+                <dt>Sumsung</dt>
+
+                <dd>SKU:</dd>
+                <dt>wh-it2900</dt>
+
+                <dd>Avability:</dd>
+                <dt>inStock</dt>
+
+                <dd>Color:</dd>
+                <dt>black</dt>
+                
+                </dl>
             </div>
 
         </section>
     )
 }
+
+export default ProductDetails
