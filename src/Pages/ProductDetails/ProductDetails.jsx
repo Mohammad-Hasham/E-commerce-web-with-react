18d332b0b56ebda12fdab68fb1208ec5dc4fee98
Mohammad-Hasham <hashamhashimi80@gmail.com>
2026-08-28 23:23:35 +0430

    product details

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,40 +1,46 @@
-function ProductDetails (){
-    return(
-        <section className="product-details">
-            <div className="imges-slide">
+function ProductDetails() {
+  return (
+    <section className="product-details">
+      <div className="imges-slide"></div>
+      <div className="product-details-img"></div>
+      <div className="product-details-informations">
+        <h2> product name </h2>
+        <div className="product-information-rate"></div>
+        <div className="product-information-price"></div>
+        <p></p>
+        <dl className="products-informations-deals">
+          <dd>Brand:</dd>
+          <dt>Sumsung</dt>
 
+          <dd>SKU:</dd>
+          <dt>wh-it2900</dt>
+
+          <dd>Avability:</dd>
+          <dt>inStock</dt>
+
+          <dd>Color:</dd>
+          <dt>black</dt>
+
+          <dd>Quantity</dd>
+          <dt>
+            <div>
+              <button>-</button>
+              <p>1</p>
+              <button>+</button>
             </div>
-            <div className="product-details-img">
-
-            </div>
-            <div className="product-details-informations">
-            <h2> product name </h2>
-            <div className="product-information-rate">
-
-            </div>
-            <div className="product-information-price">
-
-            </div>
-            <p></p>
-            <dl className="products-informations-deals">
-                <dd>Brand:</dd>
-                <dt>Sumsung</dt>
-
-                <dd>SKU:</dd>
-                <dt>wh-it2900</dt>
-
-                <dd>Avability:</dd>
-                <dt>inStock</dt>
-
-                <dd>Color:</dd>
-                <dt>black</dt>
-                {/* start  */}
-
-                </dl>
-            </div>
-
-        </section>
-    )
+          </dt>
+        </dl>
+        <div>
+          <button>Add to Cart</button>
+          <button>Buy Now</button>
+          <p>Add to Wishlist</p>
+        </div>
+      </div>
+      <div>
+    
+      </div>
+    </section>
+  );
 }
 
-export default ProductDetails
+export default ProductDetails;
