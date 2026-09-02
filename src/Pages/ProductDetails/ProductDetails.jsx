058818a364c1bd837aa4ed6bf3c3b38c293599ac
Mohammad-Hasham <hashamhashimi80@gmail.com>
2026-09-02 05:49:29 +0430

    productDetails

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,8 +1,22 @@
+import { FaTruck } from "react-icons/fa";
+import { FaClock } from "react-icons/fa";
+import { RiSecurePaymentFill } from "react-icons/ri";
+import { FaClockRotateLeft } from "react-icons/fa6";
+import "./ProductDetails.css"
+import headPhoneimg from "../../assets/images/headPhone.png"
+
 function ProductDetails() {
+
+
+  
   return (
     <section className="product-details">
-      <div className="imges-slide"></div>
-      <div className="product-details-img"></div>
+      <div className="imges-slide">
+      
+      </div>
+      <div className="product-details-img">
+        <img src={headPhoneimg} alt=""  className="product-detail-img"/>
+      </div>
       <div className="product-details-informations">
         <h2> product name </h2>
         <div className="product-information-rate"></div>
@@ -36,9 +50,29 @@ function ProductDetails() {
           <p>Add to Wishlist</p>
         </div>
       </div>
-      <div>
-        <div></div>
-    
+      <div className="features">
+        <div className="icons">
+          <span>{FaTruck}</span>
+          <div>
+            <h5>Free Shoppoing</h5>
+            <p>On order over 50$</p>
+          </div>
+          <span>{FaClock}</span>
+          <div>
+            <h5>3o Days Resturns</h5>
+            <p>Money back Gruntees</p>
+          </div>
+          <span>{RiSecurePaymentFill}</span>
+          <div>
+            <h5>Secure Paymen</h5>
+            <p>100% secure payment</p>
+          </div>
+          <span>{FaClockRotateLeft}</span>
+          <div>
+            <h5>24/7 Support</h5>
+            <p>Dedcated Support</p>
+          </div>
+        </div>
       </div>
     </section>
   );
